b992501ae45be1fe3d8f94d6a9ee5ac596e6a589
vidhi0509 <55355053+vidhi0509@users.noreply.github.com>
2021-09-19 19:53:12 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add Implementation
+def add(x,y):
+    pass
+
+# Subtract Implementation
+def subtract(x,y):
+    pass
+
+# Multiply Implementation
+def multiply(x,y):
+    pass
+
+# Divide Implementation
+def divide(x,y):
+    pass    
